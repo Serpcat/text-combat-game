7a2c469131836adfdf21ad22bc099c97a4e449b2
serp <56092798+Serpcat@users.noreply.github.com>
2022-06-21 09:31:41 -0400

    Update ML_TextCombatGame.java

--- ML_TextCombatGame.java
+++ ML_TextCombatGame.java
@@ -1,26 +1,3 @@
-/**
- * Text Combat : By Marco Luong 
- * 
- * Instructions / Notes : 
- * - Personally I find spamming attack and ultimate attack to ramp up damage in the first two floors would be helpful
- * - When your HP is low, heal up with an item and hope for the best
- * - In the boss stage, instead of ramping up more damage with ultimates, I find that it's more fun with the ultimate item (randomized spells)
- * 
- * Features : 
- * - 3 levels (ramping up in difficulty; 2 regular levels, one "boss"; SPOLIER: the boss isn't much different from the regular 
- *   mobs so no extra features other than it being super strong)
- * - Soft reset feature (when you faint and decide to go back to the first floor to train)
- * - Shop feature to buy equipment: money is gained based on HP left after the round is over, multiplied by the floor number
- * - Shield that decays
- * - Different items with different effects
- * - Ultimate forms of each action
- * 
- * Credit for borrowed code / assets : 
- * - Mr.Cohen for the general game method and play again format (do-while(playAgain()))
- * - Mr.Cohen for the valid input checkers (do-while(!validInput()))
- * - Ideas for poison, stun, critical hits from Pokemon games
- */
-
 import java.util.Scanner;
 
 public class ML_TextCombatGame
